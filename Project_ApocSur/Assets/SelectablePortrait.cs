32625d361392ef08df42d28fc4ec1fc62c16e03c
Steve Blandon <stblando@microsoft.com>
2021-08-03 19:45:34 -0400

    initial commit

---
+++ Project_ApocSur/Assets/SelectablePortrait.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectablePortrait : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    [SerializeField]
+    private Image frameOutline;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log($" {this.name} portrait selected");
+
+        if (frameOutline != null)
+        {
+            frameOutline.color = Color.white;
+        } 
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Debug.Log($" {this.name} portrait deselected");
+
+        if (frameOutline != null)
+        {
+            frameOutline.color = Color.black;
+        }
+    }
+}
