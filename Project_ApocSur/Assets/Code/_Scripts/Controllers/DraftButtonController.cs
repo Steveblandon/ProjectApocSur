15f8f8465cabd188d0bcfb32c9e6ada97926d66c
Steve Blandon <stblando@microsoft.com>
2021-12-04 16:56:05 -0500

    refactored draft button controller logic to be reusable

--- Project_ApocSur/Assets/Code/_Scripts/Controllers/DraftButtonController.cs
+++ Project_ApocSur/Assets/Code/_Scripts/Controllers/DraftButtonController.cs
@@ -1,58 +1,12 @@
 namespace Projapocsur.Scripts
 {
+    using System;
     using Projapocsur.World;
-    using UnityEngine;
 
-    [RequireComponent(typeof(ToggleUI))]
-    public class DraftButtonController : MonoBehaviour
+    public class DraftButtonController : PlayerCharacterToggleControllerBase
     {
-        public static readonly string CompName = nameof(DraftButtonController);
+        protected override IProp<bool> TrackedProp => GameMaster.Instance.PlayerCharacterSelection.IsDrafted;
 
-        private ToggleUI toggleButton;
-
-        void Start()
-        {
-            this.toggleButton = this.GetComponent<ToggleUI>();
-            this.toggleButton.OnSelectStateChangeEvent += this.OnToggleStateChangeEvent;
-            IProp<bool> draftedProp = GameMaster.Instance.PlayerCharacterSelection.IsDrafted;
-            draftedProp.ValueChangedEvent += this.OnDraftStateChangeEvent;
-            this.OnDraftStateChangeEvent(draftedProp);
-        }
-
-        void OnDestroy()
-        {
-            this.toggleButton.OnSelectStateChangeEvent -= this.OnToggleStateChangeEvent;
-            GameMaster.Instance.PlayerCharacterSelection.IsDrafted.ValueChangedEvent -= this.OnDraftStateChangeEvent;
-        }
-
-        private void OnToggleStateChangeEvent(Selectable simpleSelectable)
-        {
-            bool selecteesAreDrafted = GameMaster.Instance.PlayerCharacterSelection.IsDrafted.Value;
-
-            if (this.toggleButton.IsSelected && !selecteesAreDrafted)
-            {
-                foreach (Character character in GameMaster.Instance.PlayerCharacterSelection.All)
-                {
-                    character.IsDrafted.Value = true;
-                }
-            }
-            else if (!this.toggleButton.IsSelected && selecteesAreDrafted)
-            {
-                foreach (Character character in GameMaster.Instance.PlayerCharacterSelection.All)
-                {
-                    character.IsDrafted.Value = false;
-                }
-            }
-        }
-
-        private void OnDraftStateChangeEvent(IProp<bool> prop)
-        {
-            bool selecteesAreDrafted = GameMaster.Instance.PlayerCharacterSelection.IsDrafted.Value;
-
-            if ((this.toggleButton.IsSelected && !selecteesAreDrafted) || (!this.toggleButton.IsSelected && selecteesAreDrafted))
-            {
-                this.toggleButton.Toggle();
-            }
-        }
+        protected override Func<Character, Prop<bool>> AffectedCharacterProp => character => character.IsDrafted;
     }
 }
---
+++ Project_ApocSur/Assets/Code/_Scripts/Controllers/PlayerCharacterToggleControllerBase.cs
@@ -0,0 +1,67 @@
+﻿namespace Projapocsur.Scripts
+{
+    using System;
+    using Projapocsur.World;
+    using UnityEngine;
+
+    [RequireComponent(typeof(ToggleUI))]
+    public abstract class PlayerCharacterToggleControllerBase : MonoBehaviour
+    {
+        private const string CompName = nameof(PlayerCharacterToggleControllerBase);
+
+        private ToggleUI toggleButton;
+
+        protected abstract IProp<bool> TrackedProp { get; }
+
+        protected abstract Func<Character, Prop<bool>> AffectedCharacterProp { get; }
+
+        protected virtual void Start()
+        {
+            this.toggleButton = this.GetComponent<ToggleUI>();
+            this.toggleButton.OnSelectStateChangeEvent += this.OnToggleStateChangeEvent;
+
+            if (!this.DisableOnMissingReference(this.TrackedProp, nameof(this.TrackedProp), CompName)
+                && !this.DisableOnMissingReference(this.AffectedCharacterProp, nameof(this.AffectedCharacterProp), CompName))
+            {
+                this.TrackedProp.ValueChangedEvent += this.OnPropStateChangeEvent;
+                this.OnPropStateChangeEvent(this.TrackedProp);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            this.toggleButton.OnSelectStateChangeEvent -= this.OnToggleStateChangeEvent;
+            
+            if (this.TrackedProp != null)
+            {
+                this.TrackedProp.ValueChangedEvent -= this.OnPropStateChangeEvent;
+            }
+        }
+
+        private void OnToggleStateChangeEvent(Selectable simpleSelectable)
+        {
+            if (this.toggleButton.IsSelected && !this.TrackedProp.Value)
+            {
+                foreach (Character character in GameMaster.Instance.PlayerCharacterSelection.All)
+                {
+                    this.AffectedCharacterProp(character).Value = true;
+                }
+            }
+            else if (!this.toggleButton.IsSelected && this.TrackedProp.Value)
+            {
+                foreach (Character character in GameMaster.Instance.PlayerCharacterSelection.All)
+                {
+                    this.AffectedCharacterProp(character).Value = false;
+                }
+            }
+        }
+
+        private void OnPropStateChangeEvent(IProp<bool> prop)
+        {
+            if ((this.toggleButton.IsSelected && !this.TrackedProp.Value) || (!this.toggleButton.IsSelected && this.TrackedProp.Value))
+            {
+                this.toggleButton.Toggle();
+            }
+        }
+    }
+}
